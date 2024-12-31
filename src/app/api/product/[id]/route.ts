07d72a39cdcff7609232fe89880d73f9d3066123
mhasnain23 <tryfrelancer23@gmail.com>
2024-12-31 22:25:26 +0500

    Refactor GET function to retrieve product ID from route parameters

--- src/app/api/product/[id]/route.ts
+++ src/app/api/product/[id]/route.ts
@@ -24,10 +24,8 @@ const products = [
     },
 ];
 
-export async function GET(request: NextRequest) {
-    const { searchParams } = new URL(request.url);
-    const id = searchParams.get('id');
-    const product = products.find(p => p.id === parseInt(id || ''));
+export async function GET(request: NextRequest, { params }: { params: any }) {
+    const product = products.find(p => p.id === parseInt(params.id));
     if (product) {
         return NextResponse.json(product);
     } else {
