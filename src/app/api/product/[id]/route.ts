ac821026307aebda0aa5c8c9d2311d8ac8415184
mhasnain23 <tryfrelancer23@gmail.com>
2024-12-31 23:30:30 +0500

    niy

--- src/app/api/product/[id]/route.ts
+++ src/app/api/product/[id]/route.ts
@@ -3,40 +3,35 @@ import { NextRequest, NextResponse } from 'next/server';
 const products = [
     {
         id: 1,
-        name: 'Boys Hoodie Wear',
+        name: 'Boys Hoddie wear',
         image: "/assets/thumb1.webp",
         price: 19.99,
-        description: 'This is a boy hoodie'
+        description: 'This is a boy hoddie'
     },
     {
         id: 2,
         name: 'Men T-Shirt 2025',
         image: "/assets/thumb2.webp",
         price: 29.99,
-        description: 'This is a men t-shirt'
+        description: 'This is men t-shirt'
     },
     {
         id: 3,
-        name: "Boys Blue Jeans",
+        name: "Boys blue jeans",
         image: "/assets/thumb3.webp",
         price: 39.99,
-        description: 'This is blue jeans'
+        description: 'This is a blue jeans'
     },
 ];
 
-export async function GET(
-    request: NextRequest,
-    { params }: { params: Record<string, string> }
-) {
-    const { id } = params; // Extract `id` from `params`
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        const product = products.find(p => p.id === parseInt(params.id));
+        if (product) {
+            return NextResponse.json(product);
+        }
+    } catch (error) {
+        console.log("Internal server error", error);
 
-    const product = products.find(p => p.id === parseInt(id)); // Convert `id` to a number
-    if (product) {
-        return NextResponse.json(product); // Return the product if found
-    } else {
-        return NextResponse.json(
-            { error: 'Product not found' },
-            { status: 404 } // Return a 404 status code if the product is not found
-        );
     }
 }
